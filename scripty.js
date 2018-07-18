a1384bcbceec3bf2a81e31eb976df3b187ab50d1
Wexyz <31003505+Wexyz@users.noreply.github.com>
2018-07-18 15:04:27 +0800

    Update1

--- scripty.js
+++ scripty.js
@@ -1,16 +1,15 @@
-//  for(var y = 1; y<=4; y++){
-//  begin(y);
-//  }
-//function readURL(input){
-//  if(input.files && input.files[0]){
-//    var reader = new FileReader();
+function arrayg(array){
+ var zero = 0;
+  while(temp != 7){
+    var temp = zero;
+    for(var y=0;y<16;y++){
+      array[y] = Boolean(Math.random()>.5) ? 1 : 0;
+      temp = temp + array[y];
+      }
+  }
+  return array;
+}
 
-//    reader.onload = function(e){
-//      $('#img2').attr('src',e.target.result);
-//    };
-//    reader.readAsDataURL(input.files[0]);
-//  }
-//}
 
 function readURL(input, set){
 if(input.files && input.files[0]){
